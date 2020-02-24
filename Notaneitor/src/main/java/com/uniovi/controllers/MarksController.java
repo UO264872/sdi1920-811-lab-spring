b56cba69ab171a731b79b34bdebac75e757ab784
uo264872 <uo264872@uniovi.es>
2020-02-24 22:22:09 +0100

    SDI-SpringBoot-Sesión-4.4-Consulta

--- Notaneitor/src/main/java/com/uniovi/controllers/MarksController.java
+++ Notaneitor/src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,5 +1,6 @@
 package com.uniovi.controllers;
 
+import java.security.Principal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
 import com.uniovi.services.MarksService;
 import com.uniovi.services.UsersService;
 
@@ -43,8 +45,10 @@ public class MarksController {
 	}
 
 	@RequestMapping("/mark/list")
-	public String getList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String getList(Model model, Principal principal) {
+		String dni = principal.getName();
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 
 		return "mark/list";
 	}
@@ -91,8 +95,11 @@ public class MarksController {
 	}
 
 	@RequestMapping("mark/list/update")
-	public String updateList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String updateList(Model model, Principal principal) {
+		String dni = principal.getName();
+		User user = usersService.getUserByDni(dni);
+		
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "/mark/list :: tableMarks";
 	}
 }
